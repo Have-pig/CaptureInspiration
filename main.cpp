e15c1b6591f7a01b3a9eba4dd01c8ea0f3259a2a
Have-pig <2716378261@qq.com>
2026-07-21 22:08:37 +0800

    init

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
-#include <conio.h>
-#include <iostream>
+#include <imconfig.h>
+#include <imgui/imgui.h>
+#include <SDL3/SDL.h>
 
 int main()
 {
+    ImGui::Begin("Hello, world!");
+    ImGui::End();
 
+    return 0;
 }
 
