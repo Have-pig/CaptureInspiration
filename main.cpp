a32acf8d633a41f7521fe33ce767f0d058e4a724
Have-pig <2716378261@qq.com>
2026-07-29 23:53:18 +0800

    begin

--- main.cpp
+++ main.cpp
@@ -4,7 +4,22 @@
 
 int main()
 {
-    ImGui::Begin("Hello, world!");
+    //main window
+    ImGui::Begin("CaptureInspiration");
+    ImVec2 main_window_pos = {200, 200};
+    ImVec2 main_window_size = {800, 600};
+    ImGui::SetWindowPos("CaptureInspiration", main_window_pos);
+    ImGui::SetWindowSize("CaptureInspiration", main_window_size);
+
+
+
+
+
+
+
+
+
+
     ImGui::End();
 
     return 0;
